05b490c4f28e4efc6f37da08eae51a1ad470e968
Yago Calomino <yagoferrocalomino@gmail.com>
2023-12-03 12:49:55 -0300

    Primeiro Commit, Home/Signin/Signup/React

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import GlobalStyle from './Styles/global';
+
+const App = () => {
+    return (
+        <>
+        <div>App</div>
+        <GlobalStyle />
+        </>
+    )
+}
+
+export default App;
